e6120293b19f1d249c97aad73ae00769b2aededa
martol01 <martinl0594@gmail.com>
2018-02-26 20:48:23 +0000

    Testing reading from csv.

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ def main():
 
 def getMinimumPizzaSlice( input ):
     print(input)
+    with open( 'fileTest.csv',  'r') as openFile:
+        s = openFile.readlines()
+        for x in s:
+            values = x.strip('\n').split(',')
+            print(values)
+
+
+getMinimumPizzaSlice(1)
