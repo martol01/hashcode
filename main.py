8d34aa4766dac2ad289072d8ac3383e95d439c00
martol01 <martinl0594@gmail.com>
2018-03-01 20:27:50 +0000

    Setting up.

--- main.py
+++ main.py
@@ -18,10 +18,43 @@ class Ride():
         self.isDone  = isDone
 
 
-class Vehicle():
-    def __init__(self):
-        pass
+class Vehicle( ):
+    def __init__(self, vehicleId ):
+        self.vehicleId = vehicleId
 
 
 class DrivingManager():
-    pass
+
+    def __init__(self, numRows, numColumns, numVehicles, numRides, rideBonus, totalSteps):
+        self.numRows = numRows
+        self.numColumns = numColumns
+        self.numVehicles = numVehicles
+        self.numRides = numRides
+        self.rideBonus = rideBonus
+        self.totalSteps = totalSteps
+        self.rides = []
+        self.vehicles = []
+
+    def getRemainingSteps(self ):
+        return self.totalSteps
+
+    def reduceRemainingSteps(self, numSteps=1):
+        self.totalSteps -= numSteps
+
+    def createVehicle(self):
+        vehicle = Vehicle()
+        self.vehicles.append( vehicle )
+
+    def createRide(self, rideId, startTuple, finishTuple  ):
+        ride = Ride( rideId, startTuple, finishTuple )
+        self.rides.append( ride )
+
+    def getRide(self, rideId ):
+        for rideObj in self.rides:
+            if rideObj.rideId == rideId:
+                return rideObj
+
+    def getVehicle(self, vehicleId):
+        for vehicleObj in self.vehicles:
+            if vehicleObj.rideId == vehicleId:
+                return vehicleObj
