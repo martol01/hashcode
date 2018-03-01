5a43f71fc89b46bfd2f53cd716571548f9dce007
martol01 <martinl0594@gmail.com>
2018-03-01 20:12:05 +0000

    Setting up.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 def main():
     print('Hello World')
 
-def getMinimumPizzaSlice( input ):
-    print(input)
-    with open( 'fileTest.csv',  'r') as openFile:
-        s = openFile.readlines()
-        for x in s:
-            values = x.strip('\n').split(',')
-            print(values)
 
 
-getMinimumPizzaSlice(1)
+class Ride():
+    def __init__( self, rideId, startTuple, finishTuple ):
+        self.rideId = rideId
+        self.startTuple = startTuple
+        self.finishTuple = finishTuple
+        self.isDone = False
+        self.isAssigned = False
+
+    def setIsAssigned(self , isAssigned ):
+        self.isAssigned = isAssigned
+
+    def isDone(self , isDone ):
+        self.isDone  = isDone
+
+
+class Vehicle():
+    def __init__(self):
+        pass
+
+
+class DrivingManager():
+    pass
